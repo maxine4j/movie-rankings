d8d4e7c6471c54d836b4afbbad682f4fb05b341c
Tim <tim@tim-ings.com>
2019-05-20 03:33:42 +0800

    Adds debug keys to run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,4 @@
+set APP_SECRET_KEY=debug_key
+set FACEBOOK_CLIENTID=_____________USE_PRODUCTION_SITE_SEE_README____________
+set FACEBOOK_SECRET=____________USE_PRODUCTION_SITE_SEE_README____________
 python movie_rankings/core.py
