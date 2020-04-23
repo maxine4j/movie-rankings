70c3479852f6d796448cc186f11865de8adfb124
Tim <tim@tim-ings.com>
2020-04-23 20:19:52 +0800

    Adds reverse proxied class to wsgi app

--- movie_rankings/wsgi.py
+++ movie_rankings/wsgi.py
@@ -1,4 +1,17 @@
 from core import app as application
 
+
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get('HTTP_X_FORWARDED_PROTO')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
 if __name__ == "__main__":
+    app.wsgi_app = ReverseProxied(app.wsgi_app)
     app.run()
